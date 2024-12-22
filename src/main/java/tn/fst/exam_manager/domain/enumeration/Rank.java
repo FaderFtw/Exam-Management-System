4952180b80d2b67e33bcc7721650772cb3eaa070
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-12-22 02:08:48 +0100

    Add supervision hours to Rank enumeration

--- src/main/java/tn/fst/exam_manager/domain/enumeration/Rank.java
+++ src/main/java/tn/fst/exam_manager/domain/enumeration/Rank.java
@@ -2,14 +2,30 @@ package tn.fst.exam_manager.domain.enumeration;
 
 /**
  * The Rank enumeration.
+ * Each rank has a corresponding number of hours for supervising exams.
  */
 public enum Rank {
-    MAITRE_ASSISTANT,
-    ASSISTANT,
-    VACATAIRE,
-    MAITRE_CONF,
-    PROFESSEUR,
-    EXPERT,
-    PES,
-    CONTRACTUEL,
+    MAITRE_ASSISTANT(10),
+    ASSISTANT(8),
+    VACATAIRE(5),
+    MAITRE_CONF(12),
+    PROFESSEUR(15),
+    EXPERT(20),
+    PES(18),
+    CONTRACTUEL(6);
+
+    private final int supervisionHours;
+
+    Rank(int supervisionHours) {
+        this.supervisionHours = supervisionHours;
+    }
+
+    /**
+     * Gets the number of hours required for supervising exams.
+     *
+     * @return the number of supervision hours
+     */
+    public int getSupervisionHours() {
+        return supervisionHours;
+    }
 }
